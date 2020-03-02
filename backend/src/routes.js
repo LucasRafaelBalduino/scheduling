12990062b7476cfee1ef83cf5d2bde71c1b51cb3
Lucas Balduino <lukasrafael1220@gmail.com>
2020-03-02 10:47:15 -0300

    feat: Create Route Apponitment

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +20,7 @@ routes.post('/sessions', SessionController.store);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
