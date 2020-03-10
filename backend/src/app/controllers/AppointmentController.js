d88794dd7919251c9426b3ecc91ae2895bbd556e
Lucas Balduino <lukasrafael1220@gmail.com>
2020-03-10 12:26:50 -0300

    check validation scheduling

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -29,14 +30,41 @@ class AppointmentController {
         .json({ error: 'Your can only create appointments with providers' });
     }
 
+    /**
+     * check for past dates
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'past dates not permitted' });
+    }
+
+    /**
+     * check date availability
+     */
+
+    const chceckAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (chceckAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date in note available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.user_id,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
   }
 }
-
 export default new AppointmentController();
